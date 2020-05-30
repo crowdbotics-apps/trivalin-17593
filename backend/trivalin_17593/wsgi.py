8f2cd5f8f724cccf6e61c7dd5a9e639232754670
Crowdbotics <team@crowdbotics.com>
2020-05-30 20:50:05 +0000

    Update models

--- backend/trivalin_17593/wsgi.py
+++ backend/trivalin_17593/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trivalin_17593.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trivalin_17593.settings")
 
 application = get_wsgi_application()
